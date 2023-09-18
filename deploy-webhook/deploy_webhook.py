448a04a45b7454d8df263c1e7ce9db75c14dbab5
Tom Kralidis <tomkralidis@gmail.com>
2023-09-17 20:20:17 -0400

    update webhook payload handler, add logs

--- deploy-webhook/deploy_webhook.py
+++ deploy-webhook/deploy_webhook.py
@@ -25,10 +25,12 @@
 import hashlib
 import hmac
 import json
+import logging
 import os
 import subprocess
 
 BASEDIR = '/opt/demo.pycsw.org'
+LOGGER = logging.getLogger(__name__)
 
 
 def is_valid_secret(secret_server, secret, payload):
@@ -55,12 +57,12 @@ def is_valid_secret(secret_server, secret, payload):
 
 
 def application(environ, start_response):
-    """WSGI application to run off a Webhook"""
+    """WSGI application to update demo server from a Webhook"""
 
     error = 0
     response = {
-        'status': '400 Bad Request',
-        'message': 'Could not update repository'
+        'status': '200 OK',
+        'message': 'Success'
     }
 
     # read in request
@@ -71,35 +73,51 @@ def application(environ, start_response):
 
     try:
         request = json.loads(payload)
-    except ValueError:
+    except ValueError as err:
+        msg = 'Invalid request payload'
+        LOGGER.error(f'{msg}: {err}')
         error = 1
+        response = {
+            'status': '400 Bad Request',
+            'message': msg
+        }
 
     if error == 0:
         if (is_valid_secret(secret_key, signature, payload) and
                 not request['repository']['fork']):
-            # trigger deployment update
+
             repository_full_name = request['repository']['full_name']
+            commit_id = request['head_commit']['id']
 
             if repository_full_name == 'geopython/pycsw':
-                # update develop, install library  and touch .wsgi file to
-                os.chdir(BASEDIR)
-                subprocess.call(['docker-compose', 'down'])
-                subprocess.call(['docker', 'pull', 'geopython/pycsw:latest'])
-                subprocess.call(['git', 'pull', 'origin', 'master'])
-                subprocess.call(['docker-compose', 'up', '-d'])
+                LOGGER.debug('Triggering deployment update')
 
-            message = 'Repository {} successfully updated to {}'.format(
-                repository_full_name,
-                request['head_commit']['id'])
+                try:
+                    os.chdir(BASEDIR)
+                    subprocess.check_output(['docker-compose', 'down'])
+                    subprocess.check_output(['docker', 'pull', 'geopython/pycsw:latest'])  # noqa
+                    subprocess.check_output(['git', 'pull', 'origin', 'master'])  # noqa
+                    subprocess.check_output(['docker-compose', 'up', '-d'])
+                except subprocess.CalledProcessError as err:
+                    msg = 'Update failed'
+                    LOGGER.error(f'{msg}: {err}')
+                    response = {
+                        'status': '500 Internal Server Error',
+                        'message': msg
+                    }
+
+            msg = f'{repository_full_name}:{commit_id} successfully deployed'
 
             response = {
                 'status': '200 OK',
-                'message': message
+                'message': msg
             }
 
     output = bytes(json.dumps(response).encode('utf-8'))
 
-    response_headers = [('Content-type', 'application/json'),
+    response_headers = [('Content-Type', 'application/json'),
                         ('Content-Length', str(len(output)))]
+
     start_response(response['status'], response_headers)
+
     return [output]
